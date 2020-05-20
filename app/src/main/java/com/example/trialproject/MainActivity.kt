31d5f01446814fd7c765e246b76061bbc9cb8999
15bce102 <rohit102mittal@gmail.com>
2020-05-20 22:49:15 +0530

    updated

--- app/src/main/java/com/example/trialproject/MainActivity.kt
+++ app/src/main/java/com/example/trialproject/MainActivity.kt
@@ -1,35 +1,21 @@
 package com.example.trialproject
 
+import android.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import java.util.*
+import com.airbnb.lottie.LottieAnimationView
 
 
 class MainActivity : AppCompatActivity() {
-    var recyclerView: RecyclerView? = null
-    var adapter: RecyclerView.Adapter<*>? = null
-    private val data: List<Model>? = null
-    var mb: AppCompatButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mb = findViewById(R.id.start)
-        recyclerView = findViewById(R.id.recyclerView)
-        recyclerView!!.setHasFixedSize(true)
-        val data = ArrayList<Model>()
-        data.add(Model("Ramandeep", R.color.busy, R.drawable.demo2, R.drawable.plus))
-        data.add(Model("Ramandeep", R.color.online, R.drawable.demo2, R.drawable.plus))
-        data.add(Model("Ramandeep", R.color.offline, R.drawable.demo2, R.drawable.plus))
-        data.add(Model("Ramandeep", R.color.online, R.drawable.demo2, R.drawable.plus))
-        data.add(Model("Ramandeep", R.color.online, R.drawable.demo2, R.drawable.plus))
-        adapter = Adapter(applicationContext, data)
-        recyclerView!!.adapter = adapter
-        adapter!!.notifyDataSetChanged()
-        val llm = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
-        recyclerView!!.layoutManager = llm
+
+        val animationView = findViewById<LottieAnimationView>(R.id.av)
+        animationView.playAnimation()
     }
+
+
 }
 
